8f19dce6e365cfe1a2821c0628b252d6db5d5ae3
Minh Nguyen <nvminh@gmail.com>
2024-03-21 07:05:17 +0700

    fix showing wrong user role name in error message

--- io.openems.backend.common/src/io/openems/backend/common/metadata/User.java
+++ io.openems.backend.common/src/io/openems/backend/common/metadata/User.java
@@ -65,7 +65,7 @@ public class User extends AbstractUser {
 		}
 		var thisRole = thisRoleOpt.get();
 		if (!thisRole.isAtLeast(role)) {
-			throw OpenemsError.COMMON_ROLE_ACCESS_DENIED.exception(resource, role.toString());
+			throw OpenemsError.COMMON_ROLE_ACCESS_DENIED.exception(resource, thisRole.toString());
 		}
 		return thisRole;
 	}
