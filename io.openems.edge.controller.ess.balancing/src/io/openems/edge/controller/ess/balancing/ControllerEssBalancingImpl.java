f1284016c9162c499b4fbbbb2fcc06a3635b0be5
JimsterCoder <51102348+JimsterCoder@users.noreply.github.com>
2024-07-29 12:44:01 +1200

    remove pid control from controller balancing

--- io.openems.edge.controller.ess.balancing/src/io/openems/edge/controller/ess/balancing/ControllerEssBalancingImpl.java
+++ io.openems.edge.controller.ess.balancing/src/io/openems/edge/controller/ess/balancing/ControllerEssBalancingImpl.java
@@ -96,7 +96,8 @@ public class ControllerEssBalancingImpl extends AbstractOpenemsComponent impleme
 		/*
 		 * set result
 		 */
-		this.ess.setActivePowerEqualsWithPid(calculatedPower);
+		// this.ess.setActivePowerEqualsWithPid(calculatedPower);
+		this.ess.setReactivePowerEquals(calculatedPower);
 		this.ess.setReactivePowerEquals(0);
 	}
 
