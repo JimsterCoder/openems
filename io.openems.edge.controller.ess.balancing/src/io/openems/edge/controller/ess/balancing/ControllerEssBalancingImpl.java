86e3e6e7004f6ea0e6f453eb7b0e61549376e564
JimsterCoder <51102348+JimsterCoder@users.noreply.github.com>
2024-08-27 14:07:03 +1200

    Update ControllerEssBalancingImpl.java
    improve the control some and deal with peculiarities around 400 setting and negative power (solar)

--- io.openems.edge.controller.ess.balancing/src/io/openems/edge/controller/ess/balancing/ControllerEssBalancingImpl.java
+++ io.openems.edge.controller.ess.balancing/src/io/openems/edge/controller/ess/balancing/ControllerEssBalancingImpl.java
@@ -120,42 +120,53 @@ public class ControllerEssBalancingImpl extends AbstractOpenemsComponent impleme
 			int inverter_power = this.ess.getActivePower().getOrError();
 
 			// calculate cost on each phase
-//			double consumedPower = 0;
-			double costT = 0;
+			double consumedPower = 0;
+			costTotal = 0; // global variable used in debug function
 			for (int i = 0; i < 3; i++) {
 				if (meterW[i] > 0) {
 		          cost[i] = meterW[i] * buy / 1000.0;
-//		          consumedPower += meterW[i] + inverter_power / 3.0;
 				} else {	
 					cost[i] = meterW[i] * sell / 1000.0;
 				}
 				// total cost
-				costT += cost[i];
+				costTotal += cost[i];
+        // calculate power actually consummed by the site
+        // the meter reading plus power supplied by the inverter
+				consumedPower += meterW[i] + inverter_power / 3.0;
 			}
-			costTotal = costT;
-		
-//		  double costPerkWh;
-//		  if (consumedPower > 0) {
-//		      costPerkWh = costT * 1000.0 / consumedPower;
-//		  } else {
-//		      costPerkWh = 0;
-//		  }
 		
 		  // only act if cost is outside a set value $/kWh
 		  double costdesired = 0.04;
 		  double costrange = 0.02;
 		  double maxcost = costdesired + costrange;
 		  double mincost = costdesired - costrange;
+		  
+		  int deltapower = 100;
+		  if ((costTotal - costdesired) > 0.20) {
+			  // if the cost is more than 10c greater than desired
+			  // make a larger change
+			  deltapower = 200;
+		  }
 		
-		  if (costT > maxcost) {
+		  if (consumedPower < 0) {
+			  // if we are not consuming power, we don't need the battery
+			  // (power is coming from solar)
+			  inverter_power = 0;	
+		  }
+		  else if (costTotal > maxcost) {
 		      // increase inverter power
-		      inverter_power += 100;
+		      inverter_power += deltapower;
 		
-		  } else if (costT < mincost) {
-		      // decrease inverter power
-		  // note: we never want to buy power (inverter setting should never be negative)
-		  // this could happen when solar power exceeds consumption
-		      if (inverter_power > 100) {
+		  }
+		  else if (costTotal < mincost) {
+			  // decrease inverter power
+			  // this is an ODD problem here as the inverter can only be 0 or 400 and nothing in between
+			  // (if set to 100, it goes to 400), so if it's on 400 it will never go to 0
+			  // when it is told to set to 300, it stays at 400.
+			  if ((costTotal < 0) && (inverter_power == 400)) {
+				  inverter_power = 0;
+			  }
+			  else if (inverter_power > 100) {
 		          inverter_power -= 100;
 		      }
 		  }
