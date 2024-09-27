5721cd5a69fcf9118bfe58b1b3e6714bbe515daf
JimsterCoder <51102348+JimsterCoder@users.noreply.github.com>
2024-09-27 21:05:26 +1200

    Update ControllerEssBalancingImpl.java
    fix 'oversights' in min costing code.  got a bit carried away with situations to set the power to zero

--- io.openems.edge.controller.ess.balancing/src/io/openems/edge/controller/ess/balancing/ControllerEssBalancingImpl.java
+++ io.openems.edge.controller.ess.balancing/src/io/openems/edge/controller/ess/balancing/ControllerEssBalancingImpl.java
@@ -136,7 +136,7 @@ public class ControllerEssBalancingImpl extends AbstractOpenemsComponent impleme
 			}
 		
 		  // only act if cost is outside a set value $/kWh
-		  double costdesired = -0.005;
+		  double costdesired = -0.035;
 		  double costrange = 0.005;
 		  double maxcost = costdesired + costrange;
 		  double mincost = costdesired - costrange;
@@ -163,8 +163,8 @@ public class ControllerEssBalancingImpl extends AbstractOpenemsComponent impleme
 			  // this is an ODD problem here as the inverter can only be 0 or 400 and nothing in between
 			  // (if set to 100, it goes to 400), so if it's on 400 it will never go to 0
 			  // when it is told to set to 300, it stays at 400.
-			  if (((costTotal < 0) && (inverter_power == 400)) ||
-            ((costTotal < 0) && ((inverter_power/2.0) > consumedPower))) {
+			  if (((costTotal < mincost) && (inverter_power == 400)) ||
+            ((costTotal < mincost) && ((inverter_power * 0.4) > consumedPower))) {
 				  inverter_power = 0;
 			  }
 			  else if (inverter_power > 100) {
